f59d9edf1bdcd7669429d6b23b103c2bc1f90cb2
潘健森 <sennes111762@gmail.com>
2014-06-20 19:02:33 +0800

    update rgb

--- RGB/pwm.v
+++ RGB/pwm.v
@@ -1,6 +1,7 @@
-module PWM_LED(clk,reset,LEDG);
+module PWM_LED(clk,btn,reset,LEDG);
 
 input clk;
+input btn;
 input reset;
 output [2:0] LEDG; 
 
@@ -9,17 +10,27 @@ reg [5:0] PWM_adj;
 reg [6:0] PWM_width; 
 reg [2:0] LEDG; 
 
+always @(posedge clk or posedge reset) begin
+	if (reset) 
+		begin
+	    	counter <= 0; 
+		end
+	else if (btn) 
+		begin
+			counter <= counter+1;
+		end
+end
+
 always @(posedge clk or posedge reset)
    begin 
     if(reset)     
       begin 
-        counter <= 0; 
+
         LEDG[0] <= 0;
         LEDG[1] <= 0; 
         LEDG[2] <= 0;       
       end  
     else begin 
-       	counter <= counter+1; 
         PWM_width <= PWM_width[5:0]+ PWM_adj;    
         if(counter[26]) 
 			begin 
