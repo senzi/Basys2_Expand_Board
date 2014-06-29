dc52c9ce05aed51e94cd237431d0550ea7a354a5
潘健森 <sennes111762@gmail.com>
2014-06-29 18:50:33 +0800

    add Charlieplexing

---
+++ DEMO/Charli_6/Charlieplexing.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:49:53 06/29/2014 
+// Design Name: 
+// Module Name:    Charlieplexing 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Charlieplexing(
+    );
+
+
+endmodule
